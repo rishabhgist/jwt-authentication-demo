a01277fe09b3b25fe0174ef26cda865d3dbd4460
Rishabh Gist <damyrachit@gmail.com>
2022-11-29 15:28:35 +0530

    added userservice interface

--- src/main/java/com/niit/AuthenticationService/domain/User.java
+++ src/main/java/com/niit/AuthenticationService/domain/User.java
@@ -1,11 +1,13 @@
 package com.niit.AuthenticationService.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class User {
     @Id
+    @GeneratedValue
     private int id;
     private String firstname;
     private String lastname;
---
+++ src/main/java/com/niit/AuthenticationService/service/IUserService.java
@@ -0,0 +1,4 @@
+package com.niit.AuthenticationService.service;
+
+public interface IUserService {
+}
