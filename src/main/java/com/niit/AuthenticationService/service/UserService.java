c7cc5cfbb54549f1e5fa02f02b68712c50b3900e
Rishabh Gist <damyrachit@gmail.com>
2022-11-29 15:33:37 +0530

    added userRepository object

--- src/main/java/com/niit/AuthenticationService/service/UserService.java
+++ src/main/java/com/niit/AuthenticationService/service/UserService.java
@@ -1,10 +1,19 @@
 package com.niit.AuthenticationService.service;
 
 import com.niit.AuthenticationService.domain.User;
+import com.niit.AuthenticationService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class UserService implements IUserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<User> getAll() {
         return null;
