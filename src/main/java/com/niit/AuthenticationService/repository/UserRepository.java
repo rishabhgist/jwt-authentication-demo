4c4e989954919846b297de24449b527ec295089c
Rishabh Gist <damyrachit@gmail.com>
2022-11-29 15:25:51 +0530

    added jpaRepository implementation class

--- src/main/java/com/niit/AuthenticationService/repository/UserRepository.java
+++ src/main/java/com/niit/AuthenticationService/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.niit.AuthenticationService.repository;
 
-public interface UserRepository {
+import com.niit.AuthenticationService.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
