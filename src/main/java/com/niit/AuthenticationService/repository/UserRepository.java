8170cff19230ec0fe7aaa78fab45dabe5fd79c73
Rishabh Gist <damyrachit@gmail.com>
2022-11-29 15:31:31 +0530

    added abstract methods in user service interface

--- src/main/java/com/niit/AuthenticationService/repository/UserRepository.java
+++ src/main/java/com/niit/AuthenticationService/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.niit.AuthenticationService.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    User findByUsernameAndPassword(String username, String password);
 }
--- src/main/java/com/niit/AuthenticationService/service/IUserService.java
+++ src/main/java/com/niit/AuthenticationService/service/IUserService.java
@@ -1,4 +1,12 @@
 package com.niit.AuthenticationService.service;
 
+import com.niit.AuthenticationService.domain.User;
+
+import java.util.List;
+
 public interface IUserService {
+    List<User> getAll();
+    User save(User user);
+
+    User findByUsernameAndPassword(String username, String password);
 }
