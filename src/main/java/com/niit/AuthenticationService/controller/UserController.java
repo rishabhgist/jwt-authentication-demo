32a8c658f8dc58230734fa50a2f1bcc264c6d292
Rishabh Gist <damyrachit@gmail.com>
2022-11-29 17:47:51 +0530

    final commit

--- src/main/java/com/niit/AuthenticationService/controller/UserController.java
+++ src/main/java/com/niit/AuthenticationService/controller/UserController.java
@@ -2,22 +2,28 @@ package com.niit.AuthenticationService.controller;
 
 import com.niit.AuthenticationService.domain.User;
 import com.niit.AuthenticationService.exception.UserNotFoundException;
+import com.niit.AuthenticationService.service.JwtImp;
+import com.niit.AuthenticationService.service.SecurityTokenGenerator;
 import com.niit.AuthenticationService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping
 public class UserController {
     private UserService userService;
-
+    private SecurityTokenGenerator tokenGenerator;
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtImp tokenGenerator) {
         this.userService = userService;
+        this.tokenGenerator = tokenGenerator;
     }
-    @GetMapping("/users")
+    @GetMapping("/api/v1/users")
     public ResponseEntity<?> getAll(){
         return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
     }
@@ -27,6 +33,15 @@ public class UserController {
     }
     @GetMapping("/user")
     public ResponseEntity<?> findUser(@RequestBody User user) throws UserNotFoundException {
-        return new ResponseEntity<>(userService.getByUsernameAndPassword(user), HttpStatus.OK);
+        try{
+            userService.getByUsernameAndPassword(user);
+            Map<String, String> secretkey = new HashMap<>();
+            secretkey = tokenGenerator.generateToken(user);
+            return new ResponseEntity<>(secretkey, HttpStatus.OK);
+        }catch (UserNotFoundException e){
+            throw new UserNotFoundException();
+        }catch (Exception e){
+            return new ResponseEntity<>("Network Error !!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
