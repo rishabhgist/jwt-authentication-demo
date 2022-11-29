011f2d2c5f79d4dafc32f29240d254a6bbd5dbff
Rishabh Gist <damyrachit@gmail.com>
2022-11-29 15:32:39 +0530

    implemented UserService interface

--- src/main/java/com/niit/AuthenticationService/service/UserService.java
+++ src/main/java/com/niit/AuthenticationService/service/UserService.java
@@ -1,4 +1,22 @@
 package com.niit.AuthenticationService.service;
 
-public class UserService {
+import com.niit.AuthenticationService.domain.User;
+
+import java.util.List;
+
+public class UserService implements IUserService {
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public User findByUsernameAndPassword(String username, String password) {
+        return null;
+    }
 }
