8f04bdcb8c83c3e5f5bf955fd53b42e6ab20fe9c
Rishabh Gist <damyrachit@gmail.com>
2022-11-29 15:37:06 +0530

    added responseStatus annotation

--- src/main/java/com/niit/AuthenticationService/exception/UserNotFoundException.java
+++ src/main/java/com/niit/AuthenticationService/exception/UserNotFoundException.java
@@ -1,4 +1,8 @@
 package com.niit.AuthenticationService.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found, Invalid credentials")
 public class UserNotFoundException extends Exception{
 }
